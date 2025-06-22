fae803d28448aa51280b224e9deb1dd02bd3b21c
Reuben Frankel <rfrankel@matatika.com>
2025-06-22 03:47:59 +0100

    Add test workflow

--- tap_openweathermap/tests/test_core.py
+++ tap_openweathermap/tests/test_core.py
@@ -1,14 +1,13 @@
 """Tests standard tap features using the built-in SDK tests library."""
 
-import datetime
-
 from singer_sdk.testing import get_standard_tap_tests
 
 from tap_openweathermap.tap import TapOpenWeatherMap
 
 SAMPLE_CONFIG = {
-    "start_date": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d")
-    # TODO: Initialize minimal tap config
+    "current_weather_city_name": "london",
+    "forecast_weather_longitude": "-0.1257",
+    "forecast_weather_lattitude": "51.5085",
 }
 
 
