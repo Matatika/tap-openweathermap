cab037b469431e5aa9b9e90f696c427779b1d943
Reuben Frankel <rfrankel@matatika.com>
2025-09-02 10:33:07 +0100

    Update to newer built-in test suite

--- tap_openweathermap/tests/test_core.py
+++ tap_openweathermap/tests/test_core.py
@@ -1,6 +1,6 @@
 """Tests standard tap features using the built-in SDK tests library."""
 
-from singer_sdk.testing import get_standard_tap_tests
+from singer_sdk.testing import get_tap_test_class
 
 from tap_openweathermap.tap import TapOpenWeatherMap
 
@@ -12,11 +12,7 @@ SAMPLE_CONFIG = {
 
 
 # Run standard built-in tap tests from the SDK:
-def test_standard_tap_tests():
-    """Run standard tap tests from the SDK."""
-    tests = get_standard_tap_tests(TapOpenWeatherMap, config=SAMPLE_CONFIG)
-    for test in tests:
-        test()
-
-
-# TODO: Create additional tests as appropriate for your tap.
+TestTapFeefo = get_tap_test_class(
+    tap_class=TapOpenWeatherMap,
+    config=SAMPLE_CONFIG,
+)
